880bec554dbde122c83adaf8b9389edc19b5462b
Sasha Poplavskiy <saspoplavskiy563@gmail.com>
2023-09-01 14:24:26 +0300

    add function how add class action for tag

--- js/tab.js
+++ js/tab.js
@@ -1,37 +1,30 @@
 'use strict';
 
+function addClassAction (index){
+
+    if(!index.classList.contains('active')){
+        index.classList.add('active');
+    }
+    else{
+        index.classList.remove('active');
+    }
+}
+
 const blockBtn = document.querySelector('.btn-block'),
     btns = blockBtn.querySelectorAll('button');
 
-const infoBlock = document.querySelector('.info-block'),
-    info = document.querySelectorAll('.info');
+const info = document.querySelectorAll('.info');
 
 blockBtn.addEventListener('click', (e) => {
     //console.dir(e.target);
     if(e.target && e.target.nodeName == 'BUTTON' && e.target.outerText == 'See first'){
-        if(!info[0].classList.contains('active')){
-            info[0].classList.add('active');
-        }
-        else{
-            info[0].classList.remove('active');
-        }
-        console.log(info[0].classList.contains('active'));
+        closePrefAction(info[0], info);
     }
     else if(e.target && e.target.nodeName == 'BUTTON' && e.target.outerText == 'See second'){
-        if(!info[1].classList.contains('active')){
-            info[1].classList.add('active');
-        }
-        else{
-            info[1].classList.remove('active');
-        }
+        closePrefAction(info[1], info);
     }
     else if(e.target && e.target.nodeName == 'BUTTON' && e.target.outerText == 'See third'){
-        if(!info[2].classList.contains('active')){
-            info[2].classList.add('active');
-        }
-        else{
-            info[2].classList.remove('active');
-        }
+        closePrefAction(info[2], info);
     }
 
 });

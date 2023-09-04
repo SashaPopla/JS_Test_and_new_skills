e46184a47648edaef81c06d416008f5a6e419ef8
Sasha Poplavskiy <saspoplavskiy563@gmail.com>
2023-09-04 22:30:00 +0300

    update tab.script. add new function

--- js/script.js
+++ js/script.js
@@ -88,4 +88,37 @@ window.addEventListener('DOMContentLoaded', () => {
             console.log(block.className);
         })
     });
+
+    /*----------------------------------------------*/
+
+    const blockBtn = document.querySelector('.btn-block'),
+        btns = blockBtn.querySelectorAll('button'),
+        info = document.querySelectorAll('.info');
+
+    function hideInfoContent () {
+        info.forEach(item => {
+            item.style.display = 'none';
+            item.classList.remove('active');
+        })
+    }
+    
+    function showInfoContent (i = 0){
+        info[i].style.display = 'block';
+        info[i].classList.add('active');
+    }
+    
+    hideInfoContent();
+    showInfoContent();
+    
+    blockBtn.addEventListener('click', (e) => {
+        const target = e.target;
+        if(target && target.nodeName == 'BUTTON'){
+            btns.forEach((item, i) => {
+                if(target == item){
+                    hideInfoContent();
+                    showInfoContent(i);
+                }
+            })
+        }
+    });
 });
